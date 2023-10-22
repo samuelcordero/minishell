c68d992e17bf56f0f43b76f228ba71374f3abb13
sacorder <sacorder@student.42madrid.com>
2023-10-22 20:10:38 +0200

    builtins - work in progress

--- src/builtins.c
+++ src/builtins.c
@@ -6,9 +6,117 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:58:52 by sacorder          #+#    #+#             */
-/*   Updated: 2023/10/05 16:59:05 by sacorder         ###   ########.fr       */
+/*   Updated: 2023/10/22 20:09:42 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//faltan: export y unset, junto a gestion de env
+
+int	ft_msh_exit(t_cmd_node *node, char **envp)
+{
+	//maybe clean memory before exit
+	(void)envp;
+	if (node->args[0] && node->args[1])
+	{
+		if (node->args[2] != NULL)
+		{
+			ft_putendl_fd("MiniShell: exit: too many arguments", STDERR_FILENO);
+			return (1);
+		}
+		exit(ft_atoi(node->args[1]));
+	}
+	exit(0);
+}
+
+int	ft_change_dir(t_cmd_node *node, char **envp)
+{
+	char	*path;
+
+	if (node->args[0] && node->args[1])
+	{
+		if (node->args[2] != NULL)
+		{
+			ft_putendl_fd("MiniShell: cd: too many arguments", STDERR_FILENO);
+			return (1);
+		}
+		path = node->args[1];
+	}
+	else
+		path = "/home/samu";
+	(void)envp;
+		//it should extract home path from envp
+	if (chdir(path) == -1)
+	{
+		perror(path);
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_print_working_dir(t_cmd_node *node, char **envp)
+{
+	void	*nullable;
+	char	cwd[256];
+
+	//update cwd in eviroment!
+	(void)envp;
+	node->pid = fork();
+	if (node->pid < 0)
+		return (perror("fork"), 1);
+	if (node->pid)
+	{
+		ft_close(node->pipe_fds[1]);
+		ft_dup2(node->pipe_fds[0], STDIN_FILENO);
+		ft_close(node->pipe_fds[0]);
+	}
+	else
+	{
+		ft_close(node->pipe_fds[0]);
+		if (node->pipe_out)
+			ft_dup2(node->pipe_fds[1], STDOUT_FILENO);
+		nullable = getcwd(cwd, sizeof(cwd));
+		if (nullable)
+		{
+			perror("MiniShell: pwd");
+			return (1);
+		}
+		ft_putendl_fd(cwd, STDOUT_FILENO);
+		ft_close(node->pipe_fds[1]);
+	}
+	return (0);
+}
+
+int	ft_echo(t_cmd_node *node, char **envp)
+{
+	char	eol;
+	int		i;
+
+	(void)envp;
+	eol = 0;
+	i = 0;
+	if (node->args[1] && ft_strncmp(node->args[1], "-n", 3))
+		eol = 1;
+	node->pid = fork();
+	if (node->pid < 0)
+		return (perror("fork"), 1);
+	if (node->pid)
+	{
+		ft_close(node->pipe_fds[1]);
+		ft_dup2(node->pipe_fds[0], STDIN_FILENO);
+		ft_close(node->pipe_fds[0]);
+	}
+	else
+	{
+		ft_close(node->pipe_fds[0]);
+		if (node->pipe_out)
+			ft_dup2(node->pipe_fds[1], STDOUT_FILENO);
+		while(node->args[++i])
+			ft_putstr_fd(node->args[i], STDOUT_FILENO);
+		if (eol)
+			ft_putendl_fd("", STDOUT_FILENO);
+		ft_close(node->pipe_fds[1]);
+	}
+	return (0);
+}
