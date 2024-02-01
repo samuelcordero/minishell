7f740b24350d20adaec491eac4226fc883fdf474
Samuel Cordero <sacorder@student.42madrid.com>
2024-02-01 02:34:04 +0100

    removed old commented code for test purposes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 13:16:41 by sacorder          #+#    #+#             */
-/*   Updated: 2024/01/31 18:37:14 by sacorder         ###   ########.fr       */
+/*   Updated: 2024/02/01 02:33:09 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,62 +83,3 @@ int	main(int argc, char **argv, char **envp)
 	}
 	return (0);
 }
-
-/* FOR LEAK TESTING
-
-	atexit(leaks);
-
-void	leaks(void)
-{
-	system("leaks minishell");
-}
-
-PRINT CMD TREE
-			//print_cmdtree(m_sack.cmd_tree);
-
-void	print_cmdtree(t_cmdtree *head)
-{
-	t_cmd_node	*lst;
-	t_list		*redirs_lst;
-	int			i;
-
-	lst = head->cmd_list;
-	if ((int)head->is_logic == OR_MASK)
-		printf("\n-----OR NODE-----\n");
-	else if ((int)head->is_logic == AND_MASK)
-		printf("\n-----AND NODE-----\n");
-	else if ((int)head->is_logic == WAIT_MASK)
-		printf("\n-----WAIT NODE-----\n");
-	if (head->left)
-	{
-		printf("<LEFT (MAIN) BRANCH:>");
-		print_cmdtree(head->left);
-	}
-	if (head->right)
-	{
-		printf("<RIGHT (OPTIONAL) BRANCH:>");
-		print_cmdtree(head->right);
-	}
-	if (lst)
-		printf("\n\nCommand list:\n\n");
-	while (lst)
-	{
-		i = -1;
-		while (lst->args[++i])
-			printf("Arg %i: %s\n", i, lst->args[i]);
-		printf("Pipe in/out: %i/%i\n", (int)lst->pipe_in, (int)lst->pipe_out);
-		redirs_lst = lst->redirs_lst;
-		while (redirs_lst)
-		{
-			printf("File redir type: %i, filename %s\n",
-			(int)((t_redir_tok *)redirs_lst->content)->redir_type,
-			((t_redir_tok *)redirs_lst->content)->file_name);
-			redirs_lst = redirs_lst->next;
-		}
-		printf("\n");
-		lst = lst->next;
-	}
-}
-
-			//print_tokens(m_sack.cmd_tokens);
-*/
